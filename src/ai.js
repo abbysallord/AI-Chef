705d8eb31fc71f5b6a18076116e8693adfbf995a
Dhanush Shenoy H <dshenoyh@gmail.com>
2025-08-17 08:57:23 +0530

    Update ai.js

--- src/ai.js
+++ src/ai.js
@@ -3,7 +3,7 @@ import { InferenceClient } from '@huggingface/inference'
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page`
 
-const HF_TOKEN = "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"
+const HF_TOKEN = "your-api-key-goes-here"
 const hf = new InferenceClient(HF_TOKEN)
 
 export async function getRecipeFromMistral(ingredientsArr) {
@@ -21,4 +21,5 @@ export async function getRecipeFromMistral(ingredientsArr) {
     } catch (err) {
         console.error(err.message)
     }
+
 }
